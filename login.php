5c163059bb4954b4ec077a45f80073c17670e0ab
zav4444 <colonz@uindy.edu>
2015-11-07 13:40:08 -0500

    Created login

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php 
+define('DB_HOST', 'localhost'); 
+define('DB_NAME', 'eloyalty_data'); 
+define('DB_USER','eloyalty'); 
+define('DB_PASSWORD','@csci155'); 
+
+$con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
+$db=mysql_select_db(DB_HOST,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
+/* 
+$ID = $_POST['user']; 
+$Password = $_POST['pass']; 
+*/ 
+function SignIn() 
+{ 
+session_start(); //starting the session for user profile page if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text 
+	{ 
+	$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error()); 
+	$row = mysql_fetch_array($query) or die(mysql_error()); 
+	if(!empty($row['userName']) AND !empty($row['pass'])) 
+	{ 
+$_SESSION['userName'] = $row['pass']; 
+echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE..."; } 
+else 
+{ echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; }
+ }
+ } 
+ if(isset($_POST['submit'])) 
+ { SignIn(); } 
+?>
+
